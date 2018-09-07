8b4cc677bcb0ec567db309e3b1ab15ae720fdebc
Ratikanta <ratikanta@muvi.com>
2018-09-07 17:40:56 +0530

    This is a test comit for text 5

---
+++ src/test5.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+This is the test 5 copy oof test 1
+/*
+
+ * Copyright 2018 www.muvi.com
+
+ */
+*/This is a test by Ratikanta
+
+/**
+
+ * @author  Srikanta Kumar Sahoo <srikanta@muvi.com> 
+
+TestingGit  03-Sep-2018
+ */
+public class test1 {
+
+  //edit this file for master testing
+}
